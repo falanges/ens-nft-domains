ed3ef3938f246afde73a96875b72edf3fbcc1542
Matias Parache <mparache@emocha.com>
2022-06-01 19:29:20 -0300

    [add] First commit

---
+++ contracts/Domains.sol
@@ -0,0 +1,11 @@
+// SDPX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.10;
+
+import "hardhat/console.sol";
+
+contract Domains {
+    constructor() {
+        console.log("THE EMPTY SHELVES INSIDE OURSELVES");
+    }
+}
